ea36b94d8722435e97711c77bb12ab3e248dd1fe
Francesco Saccone <francescosaccone95@live.it>
2021-08-20 11:13:30 +0200

    implement user schema to allow user to authenticate and do crud operations

---
+++ src/models/User/schema.ts
@@ -0,0 +1,29 @@
+import { mongo, Mongoose } from "mongoose";
+import mongoose, { Schema } from "mongoose"
+
+const userSchemaFields = {
+    _id: {
+        auto: true,
+        type: mongoose.Types.ObjectId,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+}
+
+const UserSchema : Schema = new Schema(userSchemaFields,{
+    timestamps: true,
+    toObject: {
+        flattenMaps: true
+    }
+})
